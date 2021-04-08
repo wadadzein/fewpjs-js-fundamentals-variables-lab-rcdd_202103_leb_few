4f532488ca886da7e4230a9c580d3c8d8117b71e
wadad zein <wadad_zein@hotmail.com>
2021-04-08 21:58:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const companyName = 'Scuber';
-const mostProfitableNeighborhood == Chelsea;
-const companyCeo == Susan Smith;
+const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo = 'Susan Smith';
